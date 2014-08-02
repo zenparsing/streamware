1d9c17bb36cc027c38551458579a485b120614c4
zenparsing <zenparsing@gmail.com>
2014-08-02 00:42:24 -0400

    Use new module name syntax.

--- test/decoder.js
+++ test/decoder.js
@@ -1,16 +1,16 @@
 module FS from "node:fs";
 module Path from "node:path";
 
-import { File } from "package:afs";
+import { File } from "afs";
 import { NodeStream, StringDecoder, Pipe } from "../src/main.js";
 
 export async function main() {
 
     var stream = new NodeStream(FS.createReadStream(Path.resolve(__dirname, "../../es6now/build/es6now.js")));
     var decoder = new StringDecoder("utf8");
-    
+
     await Pipe.start(stream, decoder, { end: true });
-    
+
     console.log("hello");
     console.log(await decoder.read());
 }
